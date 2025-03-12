5ac2cc4d41985eb4b8343d471223d488f8edc46e
Faldiansyah <faldiansyahk@gmail.com>
2025-03-13 04:55:11 +0700

    trying api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
+import MovieList from './components/MovieList';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [movies, setMovies] = useState([
+		{
+			Title: 'Star Wars: Episode IV - A New Hope',
+			Year: '1977',
+			imdbID: 'tt0076759',
+			Type: 'movie',
+			Poster: 'https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode V - The Empire Strikes Back',
+			Year: '1980',
+			imdbID: 'tt0080684',
+			Type: 'movie',
+			Poster: 'https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster: 'https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg',
+		},
+	]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return <div>
+        <MovieList movies={movies} />
+    </div>;
 }
 
-export default App
+export default App;
